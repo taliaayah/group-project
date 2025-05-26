493517a20f5b2b4020e40faf7faf57d975b2e5bb
Ella451 <daniella.jchege1110@gmail.com>
2025-05-26 11:47:57 +0300

    Update numbers.py

--- numbers.py
+++ numbers.py
@@ -1,31 +1,33 @@
-def main():
-    numbers = []
-
-    print("Enter 10 consecutive whole numbers:")
-
-    for i in range(10):
-        while True:
-            try:
-                num = int(input(f"Enter number {i + 1}: "))
-                if i > 0 and num != numbers[i - 1] + 1:
-                    print("Number must be consecutive. Try again.")
-                    continue
-                numbers.append(num)
-                break
-            except ValueError:
-                print("Invalid input. Please enter a whole number.")
-
-    print("\nAll Numbers:")
-    print(numbers)
-
-    evens = [n for n in numbers if n % 2 == 0]
-    odds = [n for n in numbers if n % 2 != 0]
-
-    print("\nEven Numbers:")
-    print(evens)
-
-    print("\nOdd Numbers:")
-    print(odds)
-
-if _name_ == "_main_":
-    main()
+def main():  
+    numbers = []  
+  
+    print("Enter 10 consecutive whole numbers:")  
+  
+    for i in range(10):  
+        while True:  
+            try:  
+                num = int(input("Enter number " + str(i + 1) + ": "))  
+                if i > 0 and num != numbers[i - 1] + 1:  
+                    print("Number must be consecutive. Try again.")  
+                    continue  
+                numbers.append(num)  
+                break  
+            except ValueError:  
+                print("Invalid input. Please enter a whole number.")  
+  
+    print("\nAll Numbers:")  
+    print(numbers)  
+  
+    evens = [n for n in numbers if n % 2 == 0]  
+    odds = [n for n in numbers if n % 2 != 0]  
+  
+    print("\nEven Numbers:")  
+    print(evens)  
+    print("Sum of Even Numbers:", sum(evens))  
+  
+    print("\nOdd Numbers:")  
+    print(odds)  
+    print("Sum of Odd Numbers:", sum(odds))  
+  
+if __name__ == "__main__":  
+    main()  
